b5725a7eea4fe575f6dfb0d37645034b3c3ace7d
vivaldi412 <cdmyblhp@gmail.com>
2024-11-03 14:22:00 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <h1>arman</h1>
+    </div>
+  );
+}
+
+export default App;
